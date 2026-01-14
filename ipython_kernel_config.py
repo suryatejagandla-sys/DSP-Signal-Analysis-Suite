e15ecf59f59697c87be701af9d68652a084f08cd
suryatejagandla-sys <suryatejagandla@gmail.com>
2026-01-14 22:46:09 +0530

    Add comments about Butterworth filter and Nyquist criterion
    Added comments explaining the choice of Butterworth filter and Nyquist criterion.

--- ipython_kernel_config.py
+++ ipython_kernel_config.py
@@ -1,2 +1,4 @@
 c.InlineBackend.figure_formats = {"svg", "pdf"}
+# Choosing a Butterworth filter here for a maximally flat passband.
+# The sampling frequency (Fs) must be > 2 * F-cutoff to satisfy the Nyquist criterion.
 c.InlineBackend.rc = {"figure.dpi": 96}
